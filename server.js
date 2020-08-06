95448db44fb46452ddf80c056e50e6a2be81d75b
theendisfar <r3steve@gmail.com>
2020-08-06 05:54:58 -0400

    changed db name

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("*", (req, res) => {
   });
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/mern-starter", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/mern-fork-test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
